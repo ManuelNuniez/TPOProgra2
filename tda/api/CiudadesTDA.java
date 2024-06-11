2a7ff38de1be802d67e9b32e113b44de04384563
ManuelNuniez <163766190+ManuelNuniez@users.noreply.github.com>
2024-06-11 23:43:21 +0000

    Ahora ciudades vecinas devuelve un conjunto de las id de las ciudades que conectan con una ciudad

--- tda/api/CiudadesTDA.java
+++ tda/api/CiudadesTDA.java
@@ -7,8 +7,9 @@ public interface CiudadesTDA {
 	public DiccionarioSimpleStrTDA Provincias(); // devuelve provincias con sus Ids
 	public void CargarCiudad(String nombreCiudad, int idProvincia); // cambiamos string por int, sacamos el int a partir de un DiccSimple
 	public DiccionarioSimpleStrTDA Ciudades(int idProvincia); // no deberia ir un diccionario de diccionarios que nos devuelva todas las ciudades a partir de una provincia}]?
+	public void EliminarCiudades(int idCiudad);
 	
-	public DiccionarioSimpleStrTDA CiudadesVecinas(int idCiudad);
+	public ConjuntoTDA CiudadesVecinas(int idCiudad);
 	public DiccionarioSimpleStrTDA CiudadesPuente(int idOrigen, int idDestino);
 	public ConjuntoTDA CiudadesPredecesoras(int idCiudad);
 	public DiccionarioSimpleStrTDA CiudadesExtremo();
